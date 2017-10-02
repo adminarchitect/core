d9ff05894ee88e466fb2642b032ae86d64785b98
endi <endi@terranet.md>
2017-10-02 16:07:48 +0300

    show buttons conditionaly

--- publishes/views/index/media.blade.php
+++ publishes/views/index/media.blade.php
@@ -1,6 +1,6 @@
 <div id="media-{{ $id }}" class="carousel slide" data-ride="carousel" data-interval="{{ $interval }}" style="max-width: {{ $width }}px">
     <!-- Indicators -->
-    @if ($hasIndicators)
+    @if (count($media) > 1 && $hasIndicators)
     <ol class="carousel-indicators">
         @foreach($media as $item)
             <li data-target="#media-{{ $id }}" data-slide-to="{{ $loop->index }}" class="{{ (0 === $loop->index ? 'active' : '') }}"></li>
@@ -17,10 +17,10 @@
         @endforeach
     </div>
 
-    @if($hasArrows)
+    @if(count($media) > 1 && $hasArrows)
         <a class="left carousel-control" href="#media-{{ $id }}" data-slide="prev">
             <span class="glyphicon glyphicon-chevron-left"></span>
-            <span class="sr-only">{{ trans('administrator::buttons.prev') }}</span>
+            <span class="sr-only">{{ trans('administrator::buttons.previous') }}</span>
         </a>
         <a class="right carousel-control" href="#media-{{ $id }}" data-slide="next">
             <span class="glyphicon glyphicon-chevron-right"></span>
