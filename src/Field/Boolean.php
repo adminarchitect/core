f67032af152b3ad5652e72c12d2ed9e4ecf0a2b1
Endi <endi@terranet.md>
2018-09-06 21:25:40 +0300

    extend boolean type to support custom values

--- src/Field/Boolean.php
+++ src/Field/Boolean.php
@@ -4,4 +4,55 @@ namespace Terranet\Administrator\Field;
 
 class Boolean extends Generic
 {
+    /** @var mixed */
+    protected $trueValue = 1;
+
+    /** @var mixed */
+    protected $falseValue = 0;
+
+    /**
+     * @param $value
+     * @return $this
+     */
+    public function trueValue($value)
+    {
+        $this->trueValue = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param $value
+     * @return $this
+     */
+    public function falseValue($value)
+    {
+        $this->falseValue = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isTrue()
+    {
+        return $this->value() === $this->trueValue;
+    }
+
+    /**
+     * @return array
+     */
+    public function onIndex()
+    {
+        return ['isTrue' => $this->isTrue()];
+    }
+
+    /**
+     * @return array
+     */
+    public function onView()
+    {
+        return $this->onIndex();
+    }
 }
