b715ca090bae60c94deefa26b04c9115bd187779
Endi <endi@terranet.md>
2018-09-06 21:23:45 +0300

    add useful helpers

--- src/Form/Collection/Mutable.php
+++ src/Form/Collection/Mutable.php
@@ -5,6 +5,7 @@ namespace Terranet\Administrator\Form\Collection;
 use Terranet\Administrator\Collection\Mutable as BaseMutableCollection;
 use Terranet\Administrator\Columns\MediaElement;
 use Terranet\Administrator\Exception;
+use Terranet\Administrator\Field\Textarea;
 use Terranet\Administrator\Form\FormElement;
 use Terranet\Administrator\Form\FormSection;
 use Terranet\Administrator\Form\InputFactory;
@@ -89,25 +90,62 @@ class Mutable extends BaseMutableCollection
         return null !== $position ? $this->insert($section, $position) : $this->push($section);
     }
 
+    /**
+     * Whether the collection has active editor of specific type.
+     *
+     * @param $editor
+     * @return bool
+     * @throws Exception
+     */
     public function hasEditors($editor)
     {
         $this->validateEditor($editor);
 
         return (bool) $this->filter(function ($field) use ($editor) {
-            if ('ckeditor' === $editor) {
-                return $field instanceof Ckeditor;
-            }
-
-            if ('medium' === $editor) {
-                return $field instanceof Medium;
-            }
-
-            if ('markdown' === $editor) {
-                return $field instanceof Markdown;
-            }
-
-            return $field instanceof Tinymce;
+            return $field instanceof Textarea && $field->editorEnabled($editor);
         })->count();
+    }
+
+    /**
+     * Set rich editors.
+     *
+     * @param mixed string|array $fields
+     */
+    public function editors($fields, string $editor = null)
+    {
+        if (is_array($fields)) {
+            foreach ($fields as $field => $editor) {
+                $this->editors($field, $editor);
+            }
+        } elseif (is_string($fields) && $editor) {
+            $item = $this->find($fields);
+            if ($item instanceof Textarea) {
+                if (method_exists($item, $editor)) {
+                    $item->$editor();
+                }
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set fields descriptions.
+     *
+     * @param mixed string|array $fields
+     */
+    public function hints($fields, string $hint = null)
+    {
+        if (is_array($fields)) {
+            foreach ($fields as $field => $hint) {
+                $this->hints($field, $hint);
+            }
+        } elseif (is_string($fields) && $hint) {
+            $item = $this->find($fields);
+            $item->setDescription($hint);
+        }
+
+        return $this;
     }
 
     /**
