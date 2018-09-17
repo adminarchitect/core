b63ab749bf8a21a976dd6c259e023a0bac237a89
Endi <endi@terranet.md>
2018-09-17 08:31:53 +0300

    switch autocomplete off for date, daterange filters

--- publishes/views/filters/date.blade.php
+++ publishes/views/filters/date.blade.php
@@ -1,4 +1,4 @@
 <div class="input-group">
     <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
-    {{ Form::text($field->name(), $field->value(), ['class' => 'form-control', 'data-filter-type' => 'date']) }}
+    {{ Form::text($field->name(), $field->value(), ['class' => 'form-control', 'autocomplete' => 'off', 'data-filter-type' => 'date']) }}
 </div>
--- publishes/views/filters/date_range.blade.php
+++ publishes/views/filters/date_range.blade.php
@@ -1,4 +1,4 @@
 <div class="input-group">
     <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
-    {{ Form::text($field->name(), $field->value(), ['class' => 'form-control', 'data-filter-type' => 'daterange']) }}
+    {{ Form::text($field->name(), $field->value(), ['class' => 'form-control', 'autocomplete' => 'off', 'data-filter-type' => 'daterange']) }}
 </div>
