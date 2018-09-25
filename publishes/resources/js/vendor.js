d27946dade0aeac15d5a51d8bdf9c324f0923307
Endi <endi@terranet.md>
2018-09-25 09:26:13 +0300

    remove unused 3rd party components

--- publishes/resources/js/vendor.js
+++ publishes/resources/js/vendor.js
@@ -1,24 +1,5 @@
-/* jQuery */
 window.$ = window.jQuery = require('jquery');
-
-/* Moment.js */
-// window.moment = require('moment');
-
-/* Bootstrap */
-// require('@adminarchitect/bootstrap/js/tab');
-require('@adminarchitect/bootstrap/js/dropdown');
-// require('@adminarchitect/bootstrap/js/modal');
-// require('@adminarchitect/bootstrap/js/collapse');
-// require('@adminarchitect/bootstrap/js/alert');
-require('@adminarchitect/bootstrap/js/carousel');
-// require('@adminarchitect/bootstrap/js/transition');
-
-/* Date/DateRange pickers */
-// require('@adminarchitect/bootstrap-datetimepicker');
-// require('@adminarchitect/bootstrap-daterangepicker');
-
-/* jQuery Toggles */
-// require('@adminarchitect/jquery-toggles/toggles');
-
-// require('selectize');
 window.fancybox = require('fancybox')($);
+
+require('adminarchitect-mix/node_modules/bootstrap/js/dropdown');
+require('adminarchitect-mix/node_modules/bootstrap/js/carousel');
