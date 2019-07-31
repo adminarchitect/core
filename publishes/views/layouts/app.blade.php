34366f9e9977252dc339467059c9e5772b14073b
Endi <endi@terranet.md>
2019-07-31 18:49:59 +0300

    disable the global search by default

--- publishes/views/layouts/app.blade.php
+++ publishes/views/layouts/app.blade.php
@@ -32,14 +32,16 @@
             </div>
 
             <div class="headerbar">
-                <div class="searchpanel">
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Search for...">
-                        <span class="input-group-btn">
+                @if ($config->get('search'))
+                    <div class="searchpanel">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search for...">
+                            <span class="input-group-btn">
                           <button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>
                         </span>
-                    </div><!-- input-group -->
-                </div>
+                        </div><!-- input-group -->
+                    </div>
+                @endif
 
                 <div class="header-right">
                     <ul class="headermenu">
