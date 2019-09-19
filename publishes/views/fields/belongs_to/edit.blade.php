b155ea6e9f441cebba83fd2919ed7f72ebb20b73
Endi <endi@terranet.md>
2019-09-19 10:14:01 +0300

    show simple dropdown list when BelongsTo is not searchable

--- publishes/views/fields/belongs_to/edit.blade.php
+++ publishes/views/fields/belongs_to/edit.blade.php
@@ -1,4 +1,8 @@
-@if ($searchIn)
+@if (!$searchable)
+    @include('administrator::fields.enum.edit')
+@endif
+
+@if ($searchable && $searchIn)
     <instant-search
             name="{{ $field->name() }}"
             data-url="/cms/search/?searchable={{ $searchIn }}&field={{ $searchBy }}"
